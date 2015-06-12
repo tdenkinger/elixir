0be53617e7a3fa4aadf6904cc26b49216dea5805
Troy Denkinger <tdenkinger@gmail.com>
2015-06-12 08:42:28 -0400

    Make the reduce implementation private.

--- mylists.exs
+++ mylists.exs
@@ -1,12 +1,17 @@
 defmodule MyList do
   def len(list), do: _len(list, 0)
   def sum(list), do: _sum(list)
+  def reduce(list, start, func), do: _reduce(list, start, func)
 
   def map([], _func),           do: []
   def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
 
-  def reduce([], value, _), do: value
-  def reduce([head | tail], value, func), do: reduce(tail, func.(head, value), func)
+  def mapsum(list, func), do: map(list, func) |> sum(&1)
+
+  # private
+
+  defp _reduce([], value, _), do: value
+  defp _reduce([head | tail], value, func), do: _reduce(tail, func.(head, value), func)
 
   defp _len([], length),         do: length
   defp _len([_ | tail], length), do: _len(tail, length+1)
