59cb209fbe0b466bb87c77e5c9f2240d5bd31184
Troy Denkinger <troy@tunecore.com>
2015-06-20 09:56:26 -0400

    Added a mapsum function and cleaned up formatting.

--- mylists.exs
+++ mylists.exs
@@ -1,21 +1,24 @@
 defmodule MyList do
-  def len(list), do: _len(list, 0)
-  def sum(list), do: _sum(list)
+  def len(list),                 do: _len(list, 0)
+  def sum(list),                 do: _sum(list)
   def reduce(list, start, func), do: _reduce(list, start, func)
+  def map(list, func),           do: _map(list, func)
+  def mapsum(list, func),        do: map(list, func) |> sum
 
-  def map([], _func),           do: []
-  def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
+  def caesar([], _),         do: []
+  def caesar(list, rot) do
 
-  def mapsum(list, func), do: map(list, func) |> sum(&1)
+  end
 
-  # private
+  defp _map([], _func),           do: []
+  defp _map([head | tail], func), do: [ func.(head) | _map(tail, func) ]
 
-  defp _reduce([], value, _), do: value
+  defp _reduce([], value, _),               do: value
   defp _reduce([head | tail], value, func), do: _reduce(tail, func.(head, value), func)
 
   defp _len([], length),         do: length
   defp _len([_ | tail], length), do: _len(tail, length+1)
 
-  defp _sum([]), do: 0
+  defp _sum([]),            do: 0
   defp _sum([head | tail]), do: head + _sum(tail)
 end
